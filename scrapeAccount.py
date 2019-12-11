94d7a9be9d882cc328df31668247853ada950096
Neal Soni <nealsoni00@gmail.com>
2019-12-11 06:44:42 -0500

    scraper theoretically done but has a few small bugs

---
+++ scrapeAccount.py
@@ -0,0 +1,8 @@
+import process
+import scraper
+import sys
+
+if __name__ == '__main__':
+    #pass in the username of the account you want to download
+   	for i in range(1,len(sys.argv)):
+   		allData = scraper.getAccountData(sys.argv[i], True)
