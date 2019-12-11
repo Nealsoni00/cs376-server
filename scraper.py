c535696ef0ba051fd0404fc7aeee60bbc96cf44d
Neal Soni <nealsoni00@gmail.com>
2019-12-11 04:02:02 -0500

    upload as firestore as pages to get around the max page size limmit

--- scraper.py
+++ scraper.py
@@ -1,4 +1,6 @@
 import tweepy #https://github.com/tweepy/tweepy
+import re
+import time
 import io
 import requests
 import numpy as np
@@ -10,6 +12,12 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import firestore
 import twitter
 from api import KEY, API, apiObject 
+from itertools import islice
+
+def chunks(data, SIZE=10000):
+    it = iter(data)
+    for i in range(0, len(data), SIZE):
+        yield {k:data[k] for k in islice(it, SIZE)}
 
 def get_all_tweets(screen_name, getAll, api):
 
@@ -22,7 +30,7 @@ def get_all_tweets(screen_name, getAll, api):
 	alltweets = []
 
 	#make initial request for most recent tweets (200 is the maximum allowed count at each interval)
-	new_tweets = api1.user_timeline(screen_name = screen_name,count=30, include_entities=True, tweet_mode='extended')
+	new_tweets = api1.user_timeline(screen_name = screen_name,count=10, include_entities=True, tweet_mode='extended')
 
 	#save most recent tweets
 	alltweets.extend(new_tweets)
@@ -118,22 +126,24 @@ def analyse(screen_name, alltweets, apis):
 		#If the tweet is in response to another tweet, get that original tweet.
 		data = processTweet(tweet, apis,analyzer)
 		allData[tweet.id_str] = data
+		for i in range(0,50):
+			allData[tweet.id_str + str(i)] = data
 		# print(data)
 	return allData
 
 def getAccountData(screen_name, getAll = True):
 	 #convert to API objects instead of KEY objects
 	keys = []
-	keys.append(KEY( # Neal Soni api token meant for personal use
-	"devzpy79XxBxnHCZKO9NLpWdD",
-	"jJ8oGnU4ULEdubsV9s7TIfrfhORo5U3Kf3CAY0vLHTcJco2rT3",
-	"2573581272-d3PDuATbzta0XjCTTjaARdKuqCg8JmQRA8WvnjL",
-	"CP3J1KhvXa1gc1zVddcX8tAqJbylywMTAOsKYCp6iJs2h"))
 	keys.append(KEY( # Arun Soni api token meant for personal use
 	"W2rzJn96XwhdUbOVPMxRARGoY",
 	"Z0nBnpcZOvu569jkUVgBJhmyBBHuJb7c7RG1eHhTggkvyrp2ku",
 	"200493359-Z07fyvzFppn7kqtvBMzoLlGmpob7Gtqm1rXFshBH",
 	"nnj5ni1qtQj1awWfeo2JNWRi00btuukKiJOJD9zwsSNSH"))
+	keys.append(KEY( # Neal Soni api token meant for personal use
+	"devzpy79XxBxnHCZKO9NLpWdD",
+	"jJ8oGnU4ULEdubsV9s7TIfrfhORo5U3Kf3CAY0vLHTcJco2rT3",
+	"2573581272-d3PDuATbzta0XjCTTjaARdKuqCg8JmQRA8WvnjL",
+	"CP3J1KhvXa1gc1zVddcX8tAqJbylywMTAOsKYCp6iJs2h"))
 	keys.append(KEY( # Rando API token
 	"muTI4PDvkLIbsUSg7Y5iMkptp",
 	"sNCJrnABbHN4qzqGnZlsK27PiDhNPOcN8Tixc9h0RQiQsyXFQ4",
@@ -148,5 +158,9 @@ def getAccountData(screen_name, getAll = True):
 
 	alltweets = get_all_tweets(screen_name, getAll, api)
 	allData = analyse(screen_name, alltweets, api)
-	firestore.saveTweetData(screen_name, allData)
+	dataChunks = chunks(allData, 500)
+	page = 0
+	for item in dataChunks:
+		firestore.saveTweetData(screen_name, item, page)
+		page += 1
 	# print(allData)
